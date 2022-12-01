7bc99bddfda8b4f00c828cfa7c545481a50eb537
songhashin <lang_00@naver.com>
2022-12-01 17:52:57 +0900

    package 경로 변경

--- src/main/java/com/songha/projectweb/config/ProjectWebConfiguration.java
+++ src/main/java/com/songha/projectweb/config/ProjectWebConfiguration.java
@@ -1,7 +1,7 @@
 package com.songha.projectweb.config;
 
-import com.songha.projectweb.util.CategoryResolver;
-import com.songha.projectweb.util.DeviceResolver;
+import com.songha.projectweb.resolver.CategoryResolver;
+import com.songha.projectweb.resolver.DeviceResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
